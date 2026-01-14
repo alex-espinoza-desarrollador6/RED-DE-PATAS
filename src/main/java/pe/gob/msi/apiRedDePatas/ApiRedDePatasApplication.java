3ebc650cc72b6f103b747c1f2edb1aff23fa703d
Alex_Espinoza <sdsst.desarrollador6@munisanisidro.gob.pe>
2026-01-14 12:37:55 -0500

    First commit

---
+++ src/main/java/pe/gob/msi/apiRedDePatas/ApiRedDePatasApplication.java
@@ -0,0 +1,13 @@
+package pe.gob.msi.apiRedDePatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRedDePatasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRedDePatasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/gob/msi/apiRedDePatas/ApiRedDePatasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.gob.msi.apiRedDePatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRedDePatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
